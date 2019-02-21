ad72935f7d6f1238af2f86747b6574ceb7ade532
sulemankhan447 <sulemankhan447@gmail.com>
2019-02-21 13:28:42 +0530

    Created Custom Toast Layout File

--- app/src/main/java/suleman/com/customtoast/MainActivity.java
+++ app/src/main/java/suleman/com/customtoast/MainActivity.java
@@ -2,12 +2,25 @@ package suleman.com.customtoast;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnClick;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnClick = (Button)findViewById(R.id.btnClick);
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast();
+            }
+        });
+    }
+
+    private void showToast() {
+
     }
 }
