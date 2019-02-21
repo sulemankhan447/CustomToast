0c676fc72c3f1172c2874e77bfc4d44830867474
sulemankhan447 <sulemankhan447@gmail.com>
2019-02-21 14:08:54 +0530

    Completed Custom Toast

--- app/src/main/java/suleman/com/customtoast/MainActivity.java
+++ app/src/main/java/suleman/com/customtoast/MainActivity.java
@@ -2,8 +2,12 @@ package suleman.com.customtoast;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button btnClick;
@@ -21,6 +25,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void showToast() {
-
+        LayoutInflater layoutInflater = getLayoutInflater();
+        View layout = layoutInflater.inflate(R.layout.custom_toast,(ViewGroup)findViewById(R.id.toastLayout));
+        Toast toast = new Toast(this);
+        toast.setGravity(Gravity.CENTER,0,0);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(layout);
+        toast.show();
     }
 }
